dc7b31e87eacd7ac20a620f447b3d1378fcbe553
claire-d-b <claire.deberny@essca.eu>
2024-05-15 16:24:35 +0200

    Silent warnings and fix forgot password

--- web/pages/password/mail.php
+++ web/pages/password/mail.php
@@ -1,55 +1,58 @@
 <?php
-    require ('../index.php');
+  require ('../index.php');
 
-    $pdo = connect();
-    $existing = false;
-    $start = true;
+  $pdo = connect();
+  $existing = false;
+  $start = true;
 
-    if (isset($_POST['email']) && validate_input($_POST['email'], 1) == true) {
-        $start = false;
-        $email = $_POST['email'];
-        
-        $uuid = uniqid('', true);
-        // setcookie("cookies", $uuid, time()+60*60, '/');
-        setcookie("cookies", $uuid, [
-          'expires' => time() + 60*60,
-          'path' => '/',
-          'domain' => 'localhost',
-          'secure' => true,
-          'httponly' => true,
-          'samesite' => 'Strict'
-      ]);
-      
-        $sql = "SELECT * FROM profile WHERE username = (?)";
-        $data = $pdo->prepare($sql);
-        $data->bindParam(1, $email);
-        $sql = "UPDATE profile SET uuid=(?) WHERE username = (?);";
-        $profile_set_id = $pdo->prepare($sql);
-        $profile_set_id->bindParam(1, $uuid);
-        $profile_set_id->bindParam(2, $email);
-        $profile_set_id->execute();
-        $update_uuid = $data->fetch(\PDO::FETCH_ASSOC);
+  if (isset($_POST['email_user']) && validate_input($_POST['email_user'], 1) == true):
+    $start = false;
+    $email = $_POST['email_user'];
+    
+    $uuid = uniqid('', true);
+    // setcookie("cookies", $uuid, time()+60*60, '/');
+    setcookie("cookies", $uuid, [
+      'expires' => time() + 60*60,
+      'path' => '/',
+      'domain' => 'localhost',
+      'secure' => true,
+      'httponly' => true,
+      'samesite' => 'Strict'
+    ]);
 
-        if (isset($update_uuid)) {
-        $name = $update_uuid['firstname'];
-        $surname = $update_uuid['surname'];
-        $mail = $update_uuid['username'];
-        $validpass = $update_uuid['validpass'];
-        $token = md5($email.time());
+    $sql_mail = "SELECT * FROM profile WHERE username = (?);";
+    $data = $pdo->prepare($sql_mail);
+    $data->bindParam(1, $email);
+    $update_uuid = $data->fetch(\PDO::FETCH_ASSOC);
 
-        send_mail($mail, "Forgot password?",  "Reset your password", "<div>Please click on this link to reset your password: <a href='http://localhost:8080/password/index.php'>$token?activate=true</a></div>");
+    if ($update_uuid):
+      $name = $update_uuid['firstname'];
+      $surname = $update_uuid['surname'];
+      $mail = $update_uuid['username'];
+      $validpass = $update_uuid['validpass'];
+      $token = md5($email.time());
+    endif;
 
-        $del_pass_req = "UPDATE profile  SET validpass=(?) WHERE username = (?);";
-        $del_pass_data = $pdo->prepare($del_pass_req);
-        $var = null;
-        $del_pass_data->bindParam(1, $var);
-        $del_pass_data->bindParam(2, $mail);
-        $del_pass_data->execute();
+    $sql = "UPDATE profile SET uuid=(?) WHERE username = (?);";
+    $profile_set_id = $pdo->prepare($sql);
+    $profile_set_id->bindParam(1, $uuid);
+    $profile_set_id->bindParam(2, $email);
+    $profile_set_id->execute();
 
-        // header("Location: http://localhost:8080/login/index.php");
-        $existing = true;
-        }
-    } ?>
+    send_mail($mail, "Forgot password?",  "Reset your password", "<div>Please click on this link to reset your password: <a href='http://localhost:8080/password/index.php'>$token?activate=true</a></div>");
+
+    $del_pass_req = "UPDATE profile  SET validpass=(?) WHERE username = (?);";
+    $del_pass_data = $pdo->prepare($del_pass_req);
+    $var = null;
+    $del_pass_data->bindParam(1, $var);
+    $del_pass_data->bindParam(2, $mail);
+    $del_pass_data->execute();
+
+    // header("Location: http://localhost:8080/login/index.php");
+    $existing = true;
+  endif;
+?>
+
 <html dir='ltr' lang='en' style='font-family: system-ui; margin: 0; height: 100%; width: 100%;'>
   <head>
     <div style='display: flex; justify-content: center; align-items: center; width: 100%; background: white; height: 40px; position: fixed; top: 0px; box-shadow: 0px 2px 2px #808b96;'>
@@ -65,7 +68,7 @@
       <div style='width: 100%; height: 100%; margin-right: 25px; background: repeat center/10% url(../src/images/cover.png);'></div>
       <div style="width: 100%; height: 100%; display: flex; flex-direction: column; align-items: baseline; justify-content: center;">
           <p>Type your e-mail*</p>
-          <input style="border: 1px solid #7393B3; border-radius: 5px;" name="email" type="email" required="true"></input>
+          <input style="border: 1px solid #7393B3; border-radius: 5px;" name="email_user" type="email" required="true"></input>
           <div style="margin-top: 10px;"><button style="background-color: white; color: white; background-color: #7393B3; border: 1px solid white; padding: 4px; border-radius: 5px;" type="submit">Send</button></div>
           <?php if ($existing == false && $start == false): ?>
             <div style="font-weight: normal; padding-top: 10px; color: #88011f">E-mail not found</div>
